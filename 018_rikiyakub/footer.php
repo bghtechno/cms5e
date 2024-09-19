ea1d025d217d0e351bebcd657e08e2e705ac6614
rikiyakub18 <rikiyakuub@gmail.com>
2024-09-19 08:11:59 +0700

    Add files via upload
    Menambahkan Tema Wordpress

---
+++ 018_rikiyakub/footer.php
@@ -0,0 +1,8 @@
+<footer>
+<p>&copy; <?php echo date('Y'); ?> Universitas Trunojoyo Madura.</p>
+<p>Alamat: Jl. Telang No. 5, Kota Bangkalan, Indonesia</p>
+<p>Email: info@institusi.com | Telepon: (021) 123-4567</p>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
