f599226b50cfc056922394e5c600a688a39449d1
dewisangkabuana020 <dbuana45@gmail.com>
2024-09-19 08:17:45 +0700

    Add files via upload

---
+++ 020-Dewi Sangka Buana/footer.php
@@ -0,0 +1,8 @@
+<footer>
+<p>&copy; <?php echo date('Y'); ?> Nama Dewi. All rights reserved.</p>
+<p>Alamat: Jl.parikesit No. 234, Kota ponorogo, Negara Indonesia</p>
+<p>Email: dewi45@gmail.com | Telepon: 083141471645</p>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
