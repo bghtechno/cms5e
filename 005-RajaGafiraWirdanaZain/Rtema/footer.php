dc513842ef096fae7b13ace36369e5fc92ba3aa6
Rajagwz <rajagafira47@gmail.com>
2024-09-19 08:20:13 +0700

    Add files via upload
    saya menambahkan file baru

---
+++ 005-RajaGafiraWirdanaZain/Rtema/footer.php
@@ -0,0 +1,8 @@
+<footer>
+<p>&copy; <?php echo date('Y'); ?> Universitas Trunojoyo Madura. All rights reserved.</p>
+<p>Alamat: Jl. Rajagwz No. 47, Kota Bangkalan, Indonesia</p>
+<p>Email: rajagafira47.com | Telepon: 081553696387</p>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
