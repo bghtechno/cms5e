09bad5f52bd34d28e91be7819e057e65d2882dc9
nofiyantinp <nofiyantinp@gmail.com>
2024-09-19 08:13:24 +0700

    Add files via upload
    upload file pada folder saya

---
+++ 025-nofiyanti/footer.php
@@ -0,0 +1,8 @@
+<footer>
+<p>&copy; <?php echo date('Y'); ?> Dewan Kerja Cabang Sampang.</p>
+<p>Sekretariat : Jl. Pramuka No. 2, Sampang 69213</p>
+<p>Email: dkcsampang13.27@gmail.com | Telepon: (0123) 45678</p>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
