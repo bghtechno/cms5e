1bde76c2125fdb590e11f92a2c6f056958fa8adb
nailaw <nailanj203@gmail.com>
2024-09-19 08:12:23 +0700

    Add files via upload
    upload file pada folder saya

---
+++ 028-nailanataswa/mytheme/footer.php
@@ -0,0 +1,8 @@
+<footer>
+<p>&copy; <?php echo date('Y'); ?> Universitas Trunojoyo Madura. All rights reserved.</p>
+<p>Alamat: Jl. Porong No. 27, Kota Sidoarjo, Indonesia</p>
+<p>Email: nailanj203.com | Telepon: (021) 123-4567</p>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
