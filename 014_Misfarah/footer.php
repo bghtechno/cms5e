108db3c0e41cb1ecf1fb29d879b4c7361afca573
misfarah <xtkjafarah13@gmail.com>
2024-09-19 08:17:49 +0700

    Add files via upload

---
+++ 014_Misfarah/footer.php
@@ -0,0 +1,8 @@
+<footer> 
+    <p>&copy; <?php echo date('Y'); ?> Nama farah. All rights reserved.</p> 
+    <p>Alamat: Jl. telang. 44, Kota bangkalan, indonesia</p> 
+    <p>Email: farah@gmail.com | Telepon: (083) 141471645 </p> 
+</footer> 
+<?php wp_footer(); ?> 
+</body> 
+</html>
