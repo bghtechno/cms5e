5c3e9a57871eba3e062155d843e237e3d02cf93f
misfarah <xtkjafarah13@gmail.com>
2024-09-26 08:34:01 +0700

    Add files via upload
    pengumpulan tugas pekan 6

---
+++ 016-Wilda Nur Mei Syahrina/webku/footer.php
@@ -0,0 +1,8 @@
+<footer> 
+    <p>&copy; <?php echo date('Y'); ?> Syahrina. All rights reserved.</p> 
+    <p>Alamat: Jl. cumpat. 40, Kota Lamongan, indonesia</p> 
+    <p>Email: syahrinacans@gmail.com | Telepon: (083) 558324567 </p> 
+</footer> 
+<?php wp_footer(); ?> 
+</body> 
+</html>
