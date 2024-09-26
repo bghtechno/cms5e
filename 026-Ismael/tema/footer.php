b182cae6905381160971847cf59b99f943f5f008
Ismael026 <maelismael122@gmail.com>
2024-09-26 08:33:47 +0700

    Add files via upload
    mengupload file tema pekan 5

---
+++ 026-Ismael/tema/footer.php
@@ -0,0 +1,8 @@
+<footer>
+<p>&copy; <?php echo date('Y'); ?> Universitas Trunojoyo Madura. All rights reserved.</p>
+<p>Alamat: Sendang laok,kec labang, Kota Bangkalan, Indonesia</p>
+<p>Email:maelismael122@gmail.com | Telepon: (087) 739-321187</p>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
